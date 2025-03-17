ce5ece4e73885a599f467aaf57b53785300bd37c
David Alejandro Llanes Bojorquez <david.llanes1019@gmail.com>
2025-03-17 16:02:02 -0700

    Add ESLINT and Prettier config, Husky, lint-staged and Sheriff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [],
+  template: ` <h1>Hello world!!!</h1> `,
 })
 export class AppComponent {
   title = 'ng-template';
